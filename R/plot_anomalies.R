f86374d3d4db796edb6356294f4de3eecf68dad7
MartenMM <martenmiddeldorp@gmail.com>
2018-04-11 15:06:37 +0200

    Fix Error in FUN(X[[i]], …) : object 'Group' not found

--- R/plot_anomalies.R
+++ R/plot_anomalies.R
@@ -93,9 +93,10 @@ plot_anomalies.tbl_time <- function(data, time_recomposed = FALSE, ncol = 1,
 
     g <- g +
         ggplot2::geom_point(ggplot2::aes_string(color = "anomaly"), size = size_dots, alpha = alpha_dots) +
-        ggplot2::geom_point(ggplot2::aes_string(color = "anomaly"),
+        ggplot2::geom_point(ggplot2::aes_string(x = date_col, y = "observed", color = "anomaly"),
                            size = size_circles, shape = 1, alpha = alpha_circles,
-                           data = data %>% dplyr::filter(anomaly == "Yes")) +
+                           data = data %>% dplyr::filter(anomaly == "Yes"), 
+                           inherit.aes = FALSE) +
         theme_tq() +
         ggplot2::scale_color_manual(values = c("No" = color_no, "Yes" = color_yes)) +
         ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 30, hjust = 1))
