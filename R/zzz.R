06446c0a54b2f860e70a24960e72cd9298431f42
Matt Dancho <mdancho@gmail.com>
2023-02-07 11:47:48 -0500

    fix .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 
 # By default set time_scale_template_options to time_scale_template()
-# .onLoad = function(libname, pkgname) {
-#     options(
-#         time_scale_template = time_scale_template()
-#     )
-# }
-#
+.onLoad = function(libname, pkgname) {
+    options(
+        time_scale_template = time_scale_template()
+    )
+}
+
 # .onAttach <- function(libname, pkgname) {
 #
 #     bsu_rule_color <- "#2c3e50"
